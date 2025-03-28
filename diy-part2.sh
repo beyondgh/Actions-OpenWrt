373c8b1435929132626ceddf83375040a2de2903
beyondgh <64271831+beyondgh@users.noreply.github.com>
2025-03-28 23:02:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,11 +10,21 @@
 # See /LICENSE for more information.
 #
 
+git clone https://github.com/kenzok8/small-package package/smpackage
+rm -rf package/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb,luci-app-bandwidthd,luci-app-bypass,luci-app-dogcom,luci-app-gowebdav,luci-app-nginx-pingos,luci-app-ssr-plus,natflow}
+
+#因上面删除small-package仓库中的错误包里有自己想要的包，但其是错误不能使用的，逼不得已删除后使用正确的源重新添加回来。本例的helloword即对应上面被删除的ssr-plus。
+git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
+
+#增加上面仓库内没有的第三方包，有多少你想要的就增加多少个
+git clone https://github.com/kingyond/luci-app-accesscontrol-plus package/luci-app-accesscontrol-plus
+
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.50.9/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.6/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 sed -i 's/ImmortalWrt/BEYONDWrt/g' package/base-files/files/bin/config_generate
+
