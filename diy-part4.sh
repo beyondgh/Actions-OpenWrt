09aa046607e099f00807a4969838e6ddf602444f
beyondgh <64271831+beyondgh@users.noreply.github.com>
2025-04-12 02:27:31 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -17,7 +17,7 @@
 #git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
 
 #增加上面仓库内没有的第三方包，有多少你想要的就增加多少个
-#git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
+git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
 git clone https://github.com/kingyond/luci-app-accesscontrol-plus package/luci-app-accesscontrol-plus
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/sirpdboy/luci-app-advancedplus package/luci-app-advancedplus
@@ -25,7 +25,7 @@ git clone https://github.com/sirpdboy/chatgpt-web.git package/luci-app-chatgpt
 git clone https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
 git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 git clone https://github.com/riverscn/openwrt-iptvhelper package/luci-app-iptvhelper
-#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 #git clone https://github.com/Thaolga/openwrt-nekobox  package/openwrt-nekobox
 git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 git clone https://github.com/sirpdboy/luci-app-netwizard package/luci-app-netwizard
